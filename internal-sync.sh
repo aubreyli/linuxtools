3975198eaaa4ec36231014e81270e6cb225a6870
Aubrey Li <aubrey.li@intel.com>
2021-10-17 11:20:44 +0800

    Fix internal sync log file name

--- internal-sync.sh
+++ internal-sync.sh
@@ -1,10 +1,10 @@
 cd /home/aubrey/work/intel-kernel
 #sync up internal intel-kernel master branch with linux-stable linux-5.10.y branch
 git checkout master
-git pull /home/aubrey/work/linux-stable linux-5.10.y >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
+git pull /home/aubrey/work/linux-stable linux-5.10.y >> /home/aubrey/intel-kernel-`date +%Y-%m-%d`.log 2>&1
 git push >> /home/aubrey/intel-kernel-`date +%Y-%m-%d`.log 2>&1
 
 #sync up intel-kernel anck-devel-5.10 branch with cloud-kernel devel-5.10 branch
 git checkout anck-devel-5.10
-git pull /home/aubrey/anolis/cloud-kernel devel-5.10 >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
+git pull /home/aubrey/anolis/cloud-kernel devel-5.10 >> /home/aubrey/intel-kernel-`date +%Y-%m-%d`.log 2>&1
 git push >> /home/aubrey/intel-kernel-`date +%Y-%m-%d`.log 2>&1
