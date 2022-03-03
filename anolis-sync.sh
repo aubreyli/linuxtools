bb199f0282652f9a52e54ecbeab1d105a6032a22
Aubrey Li <aubrey.li@intel.com>
2022-03-03 09:22:24 +0800

    Refresh anolis daily script

--- anolis-sync.sh
+++ anolis-sync.sh
@@ -1,15 +1,38 @@
 # keep main-stream cloud-kernel up to date
+TODAY=`date +%Y-%m-%d`
+LOGFILE=/home/aubrey/anolis-pull-$TODAY.log
+EMAIL=aubrey.li@intel.com
+
 cd /home/aubrey/anolis/cloud-kernel
 git checkout devel-5.10
-git pull > /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
+#git reset --hard e2d133180bbc28a48316e67a003796885580b087 #v5.10.9
+echo "Cloud-kernel devel-5.10 branch result:" > $LOGFILE 2>&1
+echo "======================================" >>  $LOGFILE 2>&1
+git pull >> $LOGFILE 2>&1
+echo -e "\n" >>  $LOGFILE 2>&1
 
-cd /home/aubrey/anolis/intel-kernel
+git checkout linux-next
+#git reset --hard 528cecfa5af09631f0589efe9eacbd543c8c9db1 #v5.16.10
+echo "Cloud-kernel linux-next branch result:" >> $LOGFILE 2>&1
+echo "======================================" >> $LOGFILE 2>&1
+git pull >> $LOGFILE 2>&1
+echo -e "\n" >>  $LOGFILE 2>&1
+
+cd /home/aubrey/anolis/intel-cloud-kernel
 #sync up intel-kernel master branch with linux-stable master branch
-git checkout master
-git pull /home/aubrey/work/linux-stable master >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
-git push >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
+git checkout devel-5.10
+echo "Intel-cloud-kernel devel-5.10 branch result:" >> $LOGFILE 2>&1
+echo "============================================" >> $LOGFILE 2>&1
+git pull upstream devel-5.10 >> $LOGFILE 2>&1
+git push >> $LOGFILE 2>&1
+echo -e "\n" >>  $LOGFILE 2>&1
 
-#sync up intel-kernel anck-devel-5.10 branch with cloud-kernel devel-5.10 branch
-git checkout anck-devel-5.10
-git pull /home/aubrey/anolis/cloud-kernel devel-5.10 >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
-git push >> /home/aubrey/anolis-pull-`date +%Y-%m-%d`.log 2>&1
+git checkout linux-next
+echo "Intel-cloud-kernel devel-5.10 branch result:" >> $LOGFILE 2>&1
+echo "============================================" >> $LOGFILE 2>&1
+git pull upstream linux-next >> $LOGFILE 2>&1
+git push >> $LOGFILE 2>&1
+echo -e "\n" >>  $LOGFILE 2>&1
+
+cat $LOGFILE | mutt -s "[anolis]: $TODAY update" $EMAIL
+rm $LOGFILE
