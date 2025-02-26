3ffc0bd5a162ed1d6f9404de5928a5b7a9bdb8cc
Aubrey Li <aubrey.li@linux.intel.com>
2025-02-26 23:34:34 +0800

    Add common amend_patch.py

---
+++ amend_patch.py
@@ -0,0 +1,163 @@
+"""
+Amend upstream patch format for downstream community backport
+--------------------------
+Author: Aubrey Li
+"""
+
+import os
+import sys
+import re
+import argparse
+
+# Anolis Specific definition
+anbz_str     = 'ANBZ: #16164'
+# OpenCloudOS specific definition
+conflict_str = 'Conflict: none'
+# Common definition
+sig_str      = 'Backport Auto-tune per-CPU pageset size.'
+amend_str    = '[ Aubrey Li: amend commit log ]'
+sob_str      = 'Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>'
+
+def extract_commit_id(patch_text):
+    lines = patch_text.split('\n')
+    commit_id_match = re.match(r'From ([0-9a-f]{40}) ', lines[0])
+    
+    if not commit_id_match:
+        raise ValueError("Invalid patch format: Commit ID not found")
+    
+    return commit_id_match.group(1)
+
+def extract_subject_content(patch_text):
+    lines = patch_text.split('\n')
+    subject_start_idx = next((i for i, line in enumerate(lines) if line.startswith('Subject:')), None)
+    if subject_start_idx is None:
+        raise ValueError("Invalid patch format: Subject line not found")
+
+    subject_line = lines[subject_start_idx][len('Subject: '):].strip()
+
+    if (lines[subject_start_idx + 1].strip() != ''):
+        full_subject = subject_line + ' ' + lines[subject_start_idx + 1].strip()
+    else:
+        full_subject = subject_line
+    
+    subject_parts = full_subject.split()
+    if len(subject_parts) < 2:
+        raise ValueError("Invalid patch format: Subject line too short")
+    
+    return " ".join(subject_parts[2:])
+    
+def insert_commit_line(patch_text, community):
+    commit_id = extract_commit_id(patch_text)
+    lines = patch_text.split('\n')
+
+    subject_start_idx = next((i for i, line in enumerate(lines) if line.startswith('Subject:')), None)
+    if subject_start_idx is None:
+        raise ValueError("Invalid patch format: Subject line not found")
+    
+    insert_index = subject_start_idx + 1
+    while insert_index < len(lines):
+        if lines[insert_index].strip() == '':
+            break
+        insert_index += 1
+    
+    if insert_index >= len(lines):
+        raise ValueError("Invalid patch format: blank line not found")
+    else:
+        match community:
+
+            case 'anolis':
+                lines.insert(insert_index, f'commit {commit_id} upstream.')
+                lines.insert(insert_index, '')
+                lines.insert(insert_index, anbz_str)
+                lines.insert(insert_index, '')
+
+            case 'oc':
+                lines.insert(insert_index, conflict_str)
+                lines.insert(insert_index, f'[ Upstream commit {commit_id} ]')
+                lines.insert(insert_index, '')
+
+            case 'baseline':
+                lines.insert(insert_index, f'commit {commit_id} upstream.')
+                lines.insert(insert_index, '')
+
+            case _:
+                raise ValueError("Invalid community name: {community}")
+    
+    return '\n'.join(lines)
+    
+def insert_intel_sig(patch_text):
+    commit_id = extract_commit_id(patch_text)
+    subject_content = extract_subject_content(patch_text)
+    lines = patch_text.split('\n')
+
+    dash_idx = next((i for i, line in enumerate(lines) if line.strip() == '---'), None)
+    if dash_idx is None:
+        raise ValueError("Invalid patch format: blank line not found")
+    
+    insert_index = dash_idx - 1
+    while insert_index >= 0:
+        if lines[insert_index].strip() == '':
+            break
+        insert_index -= 1
+    
+    if insert_index < 0:
+        raise ValueError("Invalid patch format: blank line not found")
+    else:
+        lines.insert(insert_index, sig_str)
+        lines.insert(insert_index, f'Intel-SIG: commit {commit_id[:12]} {subject_content}.')
+        lines.insert(insert_index, '')
+    
+    return '\n'.join(lines)
+
+def insert_signature(patch_text):
+    lines = patch_text.split('\n')
+
+    insert_index = 0
+    while insert_index < len(lines):
+        if lines[insert_index].strip() == '---':
+            break
+        insert_index += 1
+
+    if insert_index >= len(lines):
+        raise ValueError("Invalid patch format: blank line not found")
+    else:
+        lines.insert(insert_index, sob_str)
+        lines.insert(insert_index, amend_str)
+    return '\n'.join(lines)
+
+def process_patch(filename, community):
+    with open(filename, "r", encoding="utf-8") as f:
+        patch_text = f.read()
+    
+    patch_text = insert_commit_line(patch_text, community)
+    patch_text = insert_intel_sig(patch_text)
+    patch_text = insert_signature(patch_text)
+    
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(patch_text)
+
+def process_directory(directory, community):
+    for filename in os.listdir(directory):
+        if filename.endswith(".patch"):  # Ensure we only process .patch files
+            file_path = os.path.join(directory, filename)
+            process_patch(file_path, community)
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Amend patch script")
+    
+    parser.add_argument("-c", "--community", required=True, help="Community name (required)")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-p", "--patch", help="Specify a patch file")
+    group.add_argument("-d", "--directory", help="Specify a directory")
+    
+    args = parser.parse_args()
+
+    if args.patch:
+        process_patch(args.patch, args.community)
+    elif args.directory:
+        process_directory(args.directory, args.community)
+
+if __name__ == "__main__":
+    main()
